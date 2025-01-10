aa00eef15ed8eb66fbbfefc4baf42e7e731f15e4
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2025-01-10 20:38:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT) || 5173, // Use the environment-provided PORT or fallback to 5173
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
 })
